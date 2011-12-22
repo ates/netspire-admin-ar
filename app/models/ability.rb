43d8376cc3f7c0f74657103c952ef5e11f4e32f9
Artem Teslenko <ates@ipv6.dp.ua>
2011-12-22 18:26:39 +0200

    Fix Ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,9 +4,7 @@ class Ability
   def initialize(user)
     can do |action, subject_class, subject|
       user.permissions.find_all_by_action(aliases_for_action(action)).any? do |permission|
-        permission.subject_class == subject_class.to_s &&
-          (subject.nil? || permission.subject_id.nil? ||
-           permission.subject_id == subject.id)
+        permission.subject_class == subject_class.to_s
       end
     end
   end
