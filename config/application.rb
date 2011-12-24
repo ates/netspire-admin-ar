ef9ca70959f61c2b5442cfed5198da90a69f499b
Artem Teslenko <ates@ipv6.dp.ua>
2011-12-24 23:49:25 +0200

    Fix CSS, add list of files for assets precompile, updated .gitignore

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module NetspireAdmin
   class Application < Rails::Application
     config.autoload_paths += %W(#{config.root}/lib)
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.assets.precompile = ['*.js', '*.css']
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
