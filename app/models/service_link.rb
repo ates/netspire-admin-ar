ecdf56f011d5f3be4f2d381e1912eeed20530182
Artem Teslenko <ates@ipv6.dp.ua>
2011-12-28 16:50:07 +0200

    Add properties for ServiceLink

--- app/models/service_link.rb
+++ app/models/service_link.rb
@@ -1,13 +1,24 @@
 class ServiceLink < ActiveRecord::Base
   belongs_to :account
   belongs_to :assigned_service
-  has_many :properties
+  has_many :properties, :dependent => :delete_all
+  has_many :options, :through => :properties
+
+  accepts_nested_attributes_for :properties
 
   validates :account_id, :presence => true
   validates :assigned_service_id, :presence => true,
                                   :uniqueness => { :scope => :account_id }
 
   before_validation :on_before_validation
+
+  def initialize_properties
+    assigned_service.service.options.each do |option|
+      if option_ids.exclude?(option.id)
+        properties.build(:option => option)
+      end
+    end
+  end
 
   protected
 
